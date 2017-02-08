88d3455b009a61e9539944293ec2f8a6579fd56c
CameronEldonGale <cameroneldongale@gmail.com>
2017-02-08 13:22:26 -0700

    make Die a dumb component

--- src/characterDice.js
+++ src/characterDice.js
@@ -10,41 +10,42 @@ class CharacterDice extends Component {
    this.state = {
                 result: 0,
                 rolled: 0,
-                input: [],
-
+                input: [0,0,0,0],
                 };
   this.handleClick = this.handleClick.bind(this)
-  // this.updateTotal = this.updateTotal.bind(this)
+  this.createDie = this.createDie.bind(this)
+  this.createDice = this.createDice.bind(this)
  }
+ createDice(inputArr){
+   function getRandomIntInclusive(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    var output =inputArr.map(function() {
+      return getRandomIntInclusive(1,6)
+    })
+    this.setState( { result: output.sort().slice(1).reduce(( acc, cur ) => acc + cur, 0 ) })
+    return output
+   }
 
  handleClick(){
-   this.setState({ rolled: this.state.rolled + 1, result: 0, input: []})
- }
- updateTotal(num){
-   this.setState((prevState, props) => {
-     prevState.input.push(num)
-  return {result: prevState.result + num , input: prevState.input.sort() }
-  });
- }
-  componentDidUpdate(prevProps, prevState){
-    if (this.state.input.length === 4) {
-      this.state.input.shift()
-      this.setState({result: this.state.input.reduce( (prev, curr) => prev + curr ) })
-      this.props.addRoll(this.state.input.reduce( (prev, curr) => prev + curr ) )
-    }
+   this.setState({ rolled: this.state.rolled + 1, input: this.createDice(this.state.input)})
 
-  }
+ }
+
+ createDie(currentValue, index, array){
+   return(
+      <Die key={index} sides='6' result={currentValue} />
+   )
+ }
 
   render(){
 
     return (
       <div className="dice-container">
       <button disabled={this.state.rolled == this.props.numberOfRolls}className="die-btn" onClick={this.handleClick}>Roll <img src={dice} className="dice" alt="dice" /></button>
-        <Die sides='6' rolled={this.state.rolled} update={this.updateTotal.bind(this)} />
-        <Die sides='6' rolled={this.state.rolled} update={this.updateTotal.bind(this)} />
-        <Die sides='6' rolled={this.state.rolled} update={this.updateTotal.bind(this)} />
-        <Die sides='6' rolled={this.state.rolled} update={this.updateTotal.bind(this)} />
-
+         {this.state.input.map(this.createDie)}
         <h2>{this.state.result}</h2>
       </div>
     );
--- src/die.js
+++ src/die.js
@@ -7,24 +7,9 @@ import './die.css';
 
 
 class Die extends Component {
-  constructor(props) {
-   super(props);
-   this.state = {
-                result: 0,
-                img: die123,
-                angle: 0,
-
-                };
- }
-
- roll(){
-   function getRandomIntInclusive(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
+  render() {
     const diceMapping = {
+      0: { img: die123, angle: '0deg' },
       1: { img: die123, angle: '0deg' },
       2: { img: die123, angle: '120deg' },
       3: { img: die123, angle: '240deg' },
@@ -32,38 +17,13 @@ class Die extends Component {
       5: { img: die456, angle: '240deg' },
       6: { img: die456, angle: '0deg' }
     }
-
-    let result = getRandomIntInclusive(1, this.props.sides)
-    this.setState({
-      result: result,
-      img: diceMapping[result]['img'],
-      angle: diceMapping[result]['angle'],
-    });
-
-
- }
-
- componentWillReceiveProps(nextProps){
-   if (nextProps.rolled !== this.props.rolled) {
-     this.roll()
-   }
- }
- componentDidUpdate(prevProps, prevState){
-   if (this.props.rolled !== prevProps.rolled) {
-     this.props.update(this.state.result)
-
-   }
- }
-
-
-  render() {
     var css = {
-      transform: "rotate("+ this.state.angle +")"
+      transform: "rotate("+ diceMapping[this.props.result]['angle'] +")"
     }
     return (
       <div className="dice-container">
-        <img src={this.state.img} className="dice" alt="die"  style={css}/>
-          <h2>{this.state.result}</h2>
+        <img src={diceMapping[this.props.result]['img']} className="dice" alt="die"  style={css}/>
+          <h2>{this.props.result}</h2>
       </div>
     );
   }
